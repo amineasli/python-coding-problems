ad914c44a480b313f7016d4cf5b151565dc5f40f
Amine Asli <amine.asli@intelcia.com>
2024-05-12 23:54:40 +0200

    Implement a stack using a class called Stack

---
+++ stack.py
@@ -0,0 +1,19 @@
+from typing import TypeVar, Generic, List
+T = TypeVar('T')
+
+class Stack(Generic[T]):
+    def __init__(self) -> None:
+        self.items: List[T] = []
+    
+    def push(self, item) -> None:
+        self.items.append(item)
+    
+    def pop(self) -> T:
+        return  self.items.pop()
+    
+    def peek(self) -> T:
+        return self.items[-1]
+    
+    def is_empty(self) -> bool:
+        #return False if self.items else True
+        return not self.items
