90a24df5c8f48f4a5be3aca5a2e426954fa902ae
Amine Asli <amine.asli@intelcia.com>
2024-05-13 00:01:51 +0200

    Solve the "Reversing a Word" problem using the stack

---
+++ reverser.py
@@ -0,0 +1,21 @@
+from stack import Stack
+
+class Reverser:
+    
+    @staticmethod
+    def do_reverse(input_str: str) -> str:
+        stack: Stack[str] = Stack()
+        output_str: str = ""
+        
+        for char in input_str:
+            stack.push(char)
+            
+        while not stack.is_empty():
+            output_str += stack.pop()
+            
+        return output_str
+        
+if __name__ == '__main__':
+    reversed_Str = Reverser.do_reverse('part')
+    print(reversed_Str)
+            
